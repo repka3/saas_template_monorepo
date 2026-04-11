8ef309d5f5b26deb80199f7fbd53b4e37c229cba
repka3 <lunardelli.alessio@gmail.com>
2026-04-11 14:50:21 +0200

    getFailResponseJson

--- apps/backend/src/controllers/publicHealthControllers.ts
+++ apps/backend/src/controllers/publicHealthControllers.ts
@@ -11,6 +11,6 @@ export const health: RequestHandler = (_req, res) => {
 
 
 export const testErrorController: RequestHandler = (_req, res) => {
-  const response = getFailResponseJson(_req,"TEST_ERROR", "This is a test error ment to be handled by frontend", {"add_infos":"This is a dummy error with additional infos information as json"})
+  const response = getFailResponseJson(_req,"TEST_ERROR", "This is a test error ment to be handled by frontend, depending on the page/situation", {"add_infos":"This is a dummy error with additional infos information as json, this is probably not really used ever, but nice to have, was usefull in one project."})
   res.status(BACKEND_ERROR).json(response)
 }
